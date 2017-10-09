0f7686e54d2b938c8f30e444e2bbbe8e22615d00
alex1rap <alex1rap@spaces.ru>
2017-10-09 14:05:42 +0300

    Конфиг класса для работы с БД

---
+++ classes/app/config/db.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: alex1rap
+ * Date: 09.10.2017
+ * Time: 14:03
+ */
+
+return [
+    "host" => "localhost",
+    "user" => "demo",
+    "pass" => "demo",
+    "base" => "demo",
+    "type" => "mysql"
+];
+ 
