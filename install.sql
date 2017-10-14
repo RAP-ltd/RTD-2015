921081d03dabbcf5f08ff4c776d1c476da40c34e
alex1rap <alex1rap@spaces.ru>
2017-10-14 17:16:58 +0300

    Changed create query for table demo.users

--- install.sql
+++ install.sql
@@ -1,5 +1,5 @@
 CREATE TABLE users (
-  id              INT         NOT NULL PRIMARY KEY,
+  id              INT         NOT NULL AUTO_INCREMENT PRIMARY KEY,
   nick            VARCHAR(20) NOT NULL UNIQUE,
   first_name      VARCHAR(20) NOT NULL DEFAULT '',
   middle_name     VARCHAR(20) NOT NULL DEFAULT '',
@@ -9,3 +9,5 @@ CREATE TABLE users (
   last_visit_time INT         NOT NULL DEFAULT 0,
   last_visit_ip   VARCHAR(39) NOT NULL DEFAULT ''
 );
+
+INSERT INTO users (nick, first_name, middle_name, last_name) VALUES ('alex1rap', 'Alexander', 'Pavlovich', 'Ryabokon');
